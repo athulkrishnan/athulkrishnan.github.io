0ec25cc863643c2761f10c5ea2697f20820b2ca8
Athul Krishnan <atk007@gmail.com>
2018-06-28 09:38:28 +0530

    finish about us

---
+++ tvasta/assets/js/main.js
@@ -0,0 +1,8 @@
+// Navbar Color & Transparency
+$(window).scroll(function () {
+if ($(window).scrollTop() >= 50) {
+$('.navbar').css('background','#333333');
+} else {
+$('.navbar').css('background','transparent');
+}
+});
